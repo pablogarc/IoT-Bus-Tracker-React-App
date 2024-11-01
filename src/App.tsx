e979b30645fdaadd09c07ce127fcbdf4ea1b6b9f
Diego Luna <diego.luna@iteso.mx>
2024-11-01 15:44:29 -0600

    Añadiendo coso de login y recarga de la tarjeta

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,24 @@ import Home from "./pages/Home/Home";
 import Map from "./pages/Map/Map";
 import Cashback from "./pages/Cashback/Cashback";
 import Security from "./pages/Security/Security";
-import { HomeIcon, MapIcon, CurrencyDollarIcon, ExclamationTriangleIcon } from '@heroicons/react/24/solid';
+import Login from "./pages/Login/Login";
+import ReloadCard from "./pages/ReloadCard/ReloadCard";
+import {
+  HomeIcon,
+  MapIcon,
+  CurrencyDollarIcon,
+  ExclamationTriangleIcon,
+  BoltIcon,
+  UserIcon,
+} from "@heroicons/react/24/solid";
 
 function App() {
   return (
     <>
+      {/* Barra de Navegación */}
       <nav className="flex justify-around p-4 bg-gray-800 text-white">
         <div>
-          <h1>IoT-Bus-Tracker-React-App</h1>
+          <h1 className="text-xl font-bold">IoT-Bus-Tracker-React-App</h1>
         </div>
         <div className="flex gap-10">
           {/* Home */}
@@ -21,13 +31,15 @@ function App() {
               Home
             </Link>
           </div>
+
           {/* Map */}
-          <div className="flex items-center hover:bg-yellow-500 hover:rounded-full transition-all">
+          <div className="flex items-center hover:bg-orange-500 hover:rounded-full transition-all">
             <Link to="/map" className="p-3 flex items-center gap-2">
-              <MapIcon className="h-5 w-5 text-yellow-400" />
+              <MapIcon className="h-5 w-5 text-orange-400" />
               Map
             </Link>
           </div>
+
           {/* Cashback */}
           <div className="flex items-center hover:bg-green-700 hover:rounded-full transition-all">
             <Link to="/cashback" className="p-3 flex items-center gap-2">
@@ -35,6 +47,7 @@ function App() {
               Cashback
             </Link>
           </div>
+
           {/* Seguridad */}
           <div className="flex items-center hover:bg-red-500 hover:rounded-full transition-all">
             <Link to="/security" className="p-3 flex items-center gap-2">
@@ -42,14 +55,33 @@ function App() {
               Seguridad
             </Link>
           </div>
+
+          {/* Login */}
+          <div className="flex items-center hover:bg-purple-500 hover:rounded-full transition-all">
+            <Link to="/login" className="p-3 flex items-center gap-2">
+              <UserIcon className="h-5 w-5 text-purple-500" />
+              Login
+            </Link>
+          </div>
+
+          {/* Recargar Tarjeta (RFID) */}
+          <div className="flex items-center hover:bg-yellow-400 hover:rounded-full transition-all">
+            <Link to="/reload" className="p-3 flex items-center gap-2">
+              <BoltIcon className="h-5 w-5 text-yellow-400" />
+              Recargar Tarjeta
+            </Link>
+          </div>
         </div>
       </nav>
 
+      {/* Rutas */}
       <Routes>
         <Route index element={<Home />} />
         <Route path="/map" element={<Map />} />
         <Route path="/cashback" element={<Cashback />} />
         <Route path="/security" element={<Security />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/reload" element={<ReloadCard />} />
       </Routes>
     </>
   );
