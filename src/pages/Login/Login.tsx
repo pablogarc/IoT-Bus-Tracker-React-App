a0c2527e9a6dd39f1a4b4c9c7b466f253fb4b1fc
Diego Luna <diego.luna@iteso.mx>
2024-11-17 23:32:32 -0600

    Arreglando padding Login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -19,15 +19,16 @@ const Login: React.FC = () => {
     };
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-dark text-white">
+        <div className="flex flex-col items-center justify-start h-full bg-dark text-white pt-4">
+            <br /><br />
             <div className="p-8 bg-gray-800 rounded-lg shadow-lg w-full max-w-lg">
-                <h2 className="text-4xl font-bold text-center text-green-500 mb-4">Iniciar Sesión</h2>
-                <p className="text-center text-lg mb-6 text-gray-200">
+                <h2 className="text-3xl font-bold text-center text-green-500 mb-3">Iniciar Sesión</h2>
+                <p className="text-center text-lg mb-5 text-gray-200">
                     Accede a tu cuenta o utiliza una de las opciones de acceso rápido.
                 </p>
 
                 {/* Botones de redes sociales */}
-                <div className="flex justify-between gap-4 mb-6">
+                <div className="flex justify-between gap-4 mb-5">
                     <button className="flex items-center justify-center w-1/5 p-3 bg-blue-600 rounded-full text-white hover:bg-blue-700 transition-all">
                         <FaFacebook size={28} />
                     </button>
@@ -46,7 +47,7 @@ const Login: React.FC = () => {
                 </div>
 
                 {/* Formulario */}
-                <div className="mb-6">
+                <div className="mb-4">
                     <label className="block text-gray-300 text-lg font-bold mb-2">Usuario</label>
                     <input
                         type="text"
@@ -56,7 +57,7 @@ const Login: React.FC = () => {
                         placeholder="Usuario"
                     />
                 </div>
-                <div className="mb-6">
+                <div className="mb-4">
                     <label className="block text-gray-300 text-lg font-bold mb-2">Contraseña</label>
                     <input
                         type="password"
@@ -74,7 +75,7 @@ const Login: React.FC = () => {
                 </button>
 
                 {/* Redirección a Registro */}
-                <p className="text-center text-gray-400 mt-4 text-lg">
+                <p className="text-center text-gray-400 mt-3 text-lg">
                     ¿No tienes una cuenta?{" "}
                     <span
                         onClick={() => navigate("/register")}
